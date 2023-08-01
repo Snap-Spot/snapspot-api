69fbfbcb264aeabcec2a58bf62843f98bea0ae6c
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-01 16:49:12 +0900

    ✨ feat: Photographer 관련 도메인 생성

---
+++ snapspot-domain/src/main/java/snap/domains/photographer/entity/PhotographerImage.java
@@ -0,0 +1,29 @@
+package snap.domains.photographer.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class PhotographerImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+
+    @ManyToOne
+    @JoinColumn(name = "photographer_id")
+    private Photographer photographer;
+
+    @Column
+    private String image;
+
+    @Builder
+    public PhotographerImage(Photographer photographer, String image) {
+        this.photographer = photographer;
+        this.image = image;
+    }
+}
