0367d7149d4a45500e9506a043c6580109eeda8d
Minseo Cho <min1130@ewhain.net>
2023-08-01 16:07:36 +0900

    ✨ feat: SecurityConfig 구현중

---
+++ snapspot-api/src/main/java/snap/api/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package snap.api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/members/test").permitAll()   // 인증 없이 접근을 허용
+                .antMatchers(HttpMethod.POST, "/members/**").authenticated()    // 접근 시 인증 필요
+                .and()
+                .build();
+    }
+}
