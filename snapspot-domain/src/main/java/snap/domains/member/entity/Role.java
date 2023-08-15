bd7c40ab64e72e2785d1d83e07c0087c0bfd0f9b
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-15 17:14:51 +0900

    🔧 fix: Role 사진작가 설명 오타 수정

--- snapspot-domain/src/main/java/snap/domains/member/entity/Role.java
+++ snapspot-domain/src/main/java/snap/domains/member/entity/Role.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
     ROLE_MEMBER("ROLE_MEMBER", "일반 고객 사용자"),
-    ROLE_PHOTOGRAPHER("ROLE_PHOTOGRAPHER", "일반 고객 사용자");
+    ROLE_PHOTOGRAPHER("ROLE_PHOTOGRAPHER", "사진 작가 사용자");
 
     private final String name;
     private final String type;
