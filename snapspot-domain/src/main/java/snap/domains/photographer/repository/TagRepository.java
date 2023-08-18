9a5addc5580eb150eb5c40f02b90dc90551bf4fc
YEJI <yeji.lee.kim@gmail.com>
2023-08-18 22:07:20 +0900

    ✨ feat: 태그 등록 기능, 특정 문자열로 시작하는 태그 조회 기능

---
+++ snapspot-domain/src/main/java/snap/domains/photographer/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package snap.domains.photographer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import snap.domains.photographer.entity.Tag;
+
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findAllByTagStartingWith(String str);
+}
--- snapspot-domain/src/main/java/snap/domains/photographer/service/PhotographerTagDomainService.java
+++ snapspot-domain/src/main/java/snap/domains/photographer/service/PhotographerTagDomainService.java
@@ -3,8 +3,11 @@ package snap.domains.photographer.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import snap.domains.photographer.entity.Photographer;
 import snap.domains.photographer.entity.PhotographerTag;
+import snap.domains.photographer.entity.Tag;
 import snap.domains.photographer.repository.PhotographerTagRepository;
+import snap.domains.photographer.repository.TagRepository;
 
 import java.util.List;
 
@@ -14,9 +17,15 @@ import java.util.List;
 public class PhotographerTagDomainService {
 
     private final PhotographerTagRepository photographerTagRepository;
+    private final TagRepository tagRepository;
 
     @Transactional(readOnly = true)
     public List<PhotographerTag> findTagList(Long photographerId){
         return photographerTagRepository.findAllByPhotographer_PhotographerId(photographerId);
+    }
+
+    public void createTag(Photographer photographer, String tag){
+        Tag entity = tagRepository.save(new Tag(tag));
+        photographerTagRepository.save(new PhotographerTag(photographer, entity));
     }
 }
