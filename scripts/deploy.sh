a1a182e6f8335553948397cf3035a186369b9012
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-02 01:20:40 +0900

    💚 feat: 작동 안하는 도커 명령어 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,20 @@
 REPOSITORY=/home/ubuntu/snapspot
 cd $REPOSITORY
 
-CURRENT_CONTAINER=$(docker ps | grep snapspot-server)
+CURRENT_CONTAINER=$(sudo docker ps)
 
 if [ -z $CURRENT_CONTAINER ]
 then
-  echo "> 현재 실행중인 도커 컨테이너가 없습니다."
-else
   echo "> kill -15 $CURRENT_CONTAINER"
-  sudo kill -15 $CURRENT_CONTAINER
-  sleep 5
+    sudo sudo docker stop snapspot-server
+    sudo sudo docker rm snapspot-server
+    sleep 5
+else
+  echo "> 현재 실행중인 도커 컨테이너가 없습니다."
 fi
 
 echo "> 도커 이미지 빌드"
 sudo docker build -t snapspot-api .
 
 echo "> 도커 컨테이너 올리기"
-sudo docker run -d docker run -d --name snapspot-server snapspot-api -p 80:8080
+sudo docker run -d --name snapspot-server -p 80:8080 snapspot-api
