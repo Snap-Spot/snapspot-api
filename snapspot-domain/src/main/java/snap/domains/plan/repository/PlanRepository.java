d5ba7c4dd4ddf7d37c0bf123ef9b906dd60e7397
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-01 21:33:45 +0900

    🔧 fix: null check 안되는 메서드 수정

--- snapspot-domain/src/main/java/snap/domains/plan/repository/PlanRepository.java
+++ snapspot-domain/src/main/java/snap/domains/plan/repository/PlanRepository.java
@@ -3,8 +3,9 @@ package snap.domains.plan.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import snap.domains.plan.entity.Plan;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-    Plan findByPlanId(UUID planId);
+public interface PlanRepository extends JpaRepository<Plan, UUID> {
+
 }
