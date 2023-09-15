feca74421267dd195829181e56b653c5b517a94e
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-15 18:24:32 +0900

    🔧 fix: 공통 enum 네이밍 변경

--- snapspot-common/src/main/java/snap/enums/Status.java
+++ snapspot-common/src/main/java/snap/enums/Status.java
@@ -8,8 +8,8 @@ public enum Status {
     DEPOSIT("STATUS_DEPOSIT", "입금 요청"),
     REFUSE("STATUS_REFUSE", "예약 거절"),
     RESERVED("STATUS_RESERVED", "예약 완료"),
-    PHOTOGRAPHING("STATUS_PHOTOGRAPHING", "촬영 진행"),
-    RETOUCHING("STATUS_RETOUCHING","사진 보정"),
+    TODAY("STATUS_TODAY", "촬영 진행"),
+    COMPLETE("STATUS_COMPLETE","촬영 완료"),
     DELIVERY("STATUS_DELIVERY", "사진 전달"),
     CANCEL("STATUS_CANCEL", "예약 취소");
 
