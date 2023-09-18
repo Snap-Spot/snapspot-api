d0db010796a8ce678ee5cf15bc81f10767a71c69
YEJI <yeji.lee.kim@gmail.com>
2023-09-18 23:36:04 +0900

    ✨ feat: 고객의 계정 정보 수정 기능

--- snapspot-api/src/main/java/snap/api/member/MemberController.java
+++ snapspot-api/src/main/java/snap/api/member/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import snap.api.member.dto.LoginRequestDto;
+import snap.api.member.dto.MemberModifyRequestDto;
 import snap.api.member.dto.MemberResponseDto;
 import snap.api.member.dto.SignupRequestDto;
 import snap.domains.member.entity.Member;
@@ -15,8 +16,15 @@ import snap.resolver.AuthMember;
 @RequestMapping("/members")
 public class MemberController {
 
+    private final MemberService memberService;
+
     @GetMapping("/me")
     public ResponseEntity<MemberResponseDto> memberInfoFind(@AuthMember Member member) {
         return new ResponseEntity<>(new MemberResponseDto(member), HttpStatus.OK);
+    }
+
+    @PutMapping("/setting")
+    public ResponseEntity<MemberResponseDto> memberUpdate(@AuthMember Member member, @RequestBody MemberModifyRequestDto requestDto){
+        return new ResponseEntity<>(memberService.updateMember(member, requestDto), HttpStatus.OK);
     }
 }
