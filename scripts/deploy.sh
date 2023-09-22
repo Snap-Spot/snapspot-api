b83a7bf3f0539b932d8160944981c7c3e6842910
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-22 14:23:36 +0900

    💚 chore: Dockerfile 생성 및 실행 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
-REPOSITORY=/home/ubuntu/snapspot
-cd $REPOSITORY
+#REPOSITORY=/home/ubuntu/snapspot
+# cd $REPOSITORY
+cd ../
 
 CURRENT_CONTAINER=$(sudo docker ps)
 
@@ -8,14 +9,21 @@ then
   echo "> 현재 실행중인 도커 컨테이너가 없습니다."
 else
   echo "> kill -15 $CURRENT_CONTAINER"
-  sudo sudo docker stop snapspot-server
+  sudo docker stop snapspot-server
+  sudo docker stop snapspot-batch-server
   sleep 10
-  sudo sudo docker rm snapspot-server
+  sudo docker rm snapspot-server
+  sudo docker rm snapspot-batch-server
   sleep 5
 fi
 
-echo "> 도커 이미지 빌드"
+echo "> 도커 이미지 빌드: api"
 sudo docker build -t snapspot-api .
 
-echo "> 도커 컨테이너 올리기"
-sudo docker run -d -p 80:8080 --name snapspot-server snapspot-api 
+echo "> snapspot-api 도커 컨테이너 올리기"
+sudo docker run -d -p 80:8080 --name snapspot-server snapspot-api
+
+echo "> 도커 이미지 빌드: batch"
+cd ./snapspot-batch
+sudo docker build -t snapspot-batch .
+sudo docker run -d -p 8080:8080 --name snapspot-batch-server snapspot-batch
