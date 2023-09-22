53876bbd05a9219932506f1c293bfc9838ad350e
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-22 14:24:05 +0900

    💚 chore: spring application 실행 전에 한국 타임존 설정

--- snapspot-api/src/main/java/snap/ApiApplication.java
+++ snapspot-api/src/main/java/snap/ApiApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EntityScan(basePackages = {"snap"})
 @EnableJpaRepositories(basePackages = {"snap"})
 @SpringBootApplication
 public class ApiApplication {
+
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
