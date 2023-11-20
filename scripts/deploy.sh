8701c800547032314e675a094874bde950effff5
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2023-11-20 12:07:29 +0900

    CHORE: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,16 @@
-REPOSITORY=/home/ubuntu/snapspot
+REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-CURRENT_CONTAINER=$(sudo docker ps)
+CURRENT_CONTAINER=$(sudo docker ps -aq --filter "name=qcard")
 
 if [ -z $CURRENT_CONTAINER ]
 then
   echo "> 현재 실행중인 도커 컨테이너가 없습니다."
 else
-  echo "> kill -15 $CURRENT_CONTAINER"
-  sudo docker-compose down
-  sudo docker rm -f $(sudo docker ps -aq)
-  sudo docker rmi $(sudo docker images -q)
+  echo "> kill  $CURRENT_CONTAINER"
+  sudo docker rm -f $CURRENT_CONTAINER
+  sudo docker rmi $CURRENT_CONTAINER
 fi
 
-echo "> 도커 이미지 빌드: api"
-sudo docker-compose up -d
-
-echo "> 도커 이미지 빌드: batch"
-cd ./snapspot-batch
-sudo docker build -t snapspot-batch .
-sudo docker run -d -p 8080:8080 --name snapspot-batch-server snapspot-batch
+sudo docker build -t qcard .
+sudo docker run -d -p 80:8080 --name qcard --network app qcard
